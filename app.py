53c5f78de53cabad66d65994e6eba0c7913ac095
naveedahmedexpert <naveedahmedexpert@gmail.com>
2025-05-15 20:04:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! From Naveed Expert and Abrar Ahmed"
+    return "Hello, World! From Naveed Expert and Abrar Ahmed and Arwa"
+
 
 
 
